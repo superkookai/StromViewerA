df102bd85c9fea52cfe90d33e59eb5ffa088daa6
superkookai <weerawut@live.com>
2025-12-28 10:57:10 +0700

    Initial Commit

---
+++ StromViewerA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StromViewerA
+//
+//  Created by Weerawut on 28/12/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StromViewerA/StromViewerAApp.swift
@@ -0,0 +1,17 @@
+//
+//  StromViewerAApp.swift
+//  StromViewerA
+//
+//  Created by Weerawut on 28/12/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct StromViewerAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
